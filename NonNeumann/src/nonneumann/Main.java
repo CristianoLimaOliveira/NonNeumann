4b5250c9cf5098816893508789ab5d5015278646
cristiano <59293960+CristianoLimaOliveira@users.noreply.github.com>
2021-01-05 16:49:35 -0300

    Início da implementação do Analisador Sintático
    Implementadas as produções para realizar a estruturação das funções, e
    também os seus respectivos parâmetros e assinaturas.

--- NonNeumann/src/nonneumann/Main.java
+++ NonNeumann/src/nonneumann/Main.java
@@ -1,8 +1,10 @@
 package nonneumann;
-import nonneumann.lexer.*;
-import nonneumann.node.*;
 
 import java.io.*;
+
+import nonneumann.lexer.*;
+import nonneumann.node.*;
+import nonneumann.parser.*;
 
 public class Main
 {
@@ -12,15 +14,24 @@ public class Main
 		{
 			String arquivo = "teste/teste.non";
 
-			Lexer lexer =
+			Lexer lex =
 					new Lexer(
 							new PushbackReader(  
 									new FileReader(arquivo), 1024)); 
-			Token token;
+			Parser p = new Parser(lex); 
+			   
+		    Start tree = p.parse();
+		    //Imprime √°rvore na sa√≠da padr√£o
+		    //tree.apply(new ASTPrinter());
+		    //Imprime √°rvore em interface gr√°fica
+		    tree.apply(new ASTDisplay());
+		    
+		    //Coment·rio abaixo relacionado ‡ fase de analisador lÈxico
+			/*Token token;
 			while(!((token = lexer.next()) instanceof EOF)) {
 				System.out.print(token.getClass());
 				System.out.println(" ( "+token.toString()+")");
-			}
+			}*/
 		}
 		catch(Exception e)
 		{
